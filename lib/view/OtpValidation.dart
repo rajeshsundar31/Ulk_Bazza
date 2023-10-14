e2051af3f617a3c08d12f63f03d1018682cda624
RajeshWaran Senthilkumar <71629749+rajeshsundar31@users.noreply.github.com>
2023-10-14 17:14:23 +0530

    Update firebase otpvalidation

--- lib/view/OtpValidation.dart
+++ lib/view/OtpValidation.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:otp_text_field/otp_text_field.dart';
 import 'package:otp_text_field/style.dart';
 import 'package:ulk_bazza/commoncomponent/Colors.dart';
 import 'package:ulk_bazza/view/Base.dart';
+import 'package:ulk_bazza/view/mobileValidation.dart';
 
 class OtpValidation extends StatefulWidget {
   const OtpValidation({super.key});
@@ -14,6 +16,8 @@ class OtpValidation extends StatefulWidget {
 
 class _OtpValidationState extends State<OtpValidation> {
   var argu = Get.arguments;
+
+  final FirebaseAuth auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -52,7 +56,12 @@ class _OtpValidationState extends State<OtpValidation> {
               style: const TextStyle(fontSize: 20),
               textFieldAlignment: MainAxisAlignment.spaceAround,
               fieldStyle: FieldStyle.underline,
-              onCompleted: (pin) {
+              onCompleted: (pin) async{
+                PhoneAuthCredential credential = PhoneAuthProvider.credential(
+                    verificationId: MobileValidation.verifiy, smsCode: pin);
+
+                // Sign the user in (or link) with the credential
+                await auth.signInWithCredential(credential);
                 if (pin.length == 5){
                   Get.to(() => const ProductBase());
                 }
